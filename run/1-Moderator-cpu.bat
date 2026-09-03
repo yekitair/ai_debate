2906ef87e8a2a4ad18c2e26e2c323ac6bf922d84
yekitair <63094206+yekitair@users.noreply.github.com>
2026-09-03 22:32:21 +0400

    Add final Moderator launch script

---
+++ run/1-Moderator-cpu.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\VULKAN\llama-server.exe -m "C:\Users\Administrateur\Desktop\GGUF\gemma4Coding-12B-Q4_K_M.gguf" -ngl 0 -t 12 -c 10240 -fa on -b 2048 --host 0.0.0.0 --port 8081 -np 1
+pause
