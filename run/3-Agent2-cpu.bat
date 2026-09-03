6893274f3b515ec398788e74846be5bf01e310c1
yekitair <63094206+yekitair@users.noreply.github.com>
2026-09-04 00:02:50 +0400

    Delete run directory

--- run/3-Agent2-cpu.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\VULKAN\llama-server.exe -m "C:\Users\Administrateur\Desktop\GGUF\qwen2.5-coder-7b-instruct-q6_k.gguf" -ngl 0 -t 12 -c 10240 -fa on -b 2048 --host 0.0.0.0 --port 8082 -np 1
-pause
